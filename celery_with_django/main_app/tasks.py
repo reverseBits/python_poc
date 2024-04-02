3a585fab5041ed2fce3ce24aac0a9759dad1e46e
dipali <dipali.shimpi@reversebits.tech>
2024-04-02 20:48:11 +0530

    added all changes in PR

--- celery_with_django/main_app/tasks.py
+++ celery_with_django/main_app/tasks.py
@@ -2,11 +2,11 @@ from celery import shared_task
 from time import sleep
 
 @shared_task(bind=True)
-def add(self,x,y):
+def add(self,x:int,y:int) -> int:
     sleep(60)
     return x + y
 
 @shared_task(bind=True)
-def sub(self,x,y):
+def sub(self,x:int,y:int) -> int:
     sleep(120)
     return x - y
--- celery_with_django/main_app/views.py
+++ celery_with_django/main_app/views.py
@@ -3,7 +3,10 @@ from .tasks import add, sub
 from send_mail_app.tasks import send_mail_func
 
 def test(request):
-    result_add = add.delay(10,20)
+    # In Celery, the .delay() method is used to enqueue a task for asynchronous execution. 
+    # When you define a Celery task using the @shared_task decorator, 
+    # you essentially create a task class with methods like .delay() to enqueue the task.
+    result_add = add.delay(10,20) 
     result_sub = sub.delay(20,5)
     print("result of add , result of sub", result_add.status, result_sub.status)
     return HttpResponse("Queue add  and queue sub")

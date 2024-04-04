0b27d1908524b11197e97060b3a9910bb8663952
reverseBits <tech.reversebits@gmail.com>
2024-04-04 16:37:42 +0530

    Update celery_with_django/main_app/views.py
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- celery_with_django/main_app/views.py
+++ celery_with_django/main_app/views.py
@@ -6,7 +6,7 @@ def test(request):
     # In Celery, the .delay() method is used to enqueue a task for asynchronous execution.
     # When you define a Celery task using the @shared_task decorator,
     # you essentially create a task class with methods like .delay() to enqueue the task.
-    result_add = add.delay(10,20) 
+    result_add = add.delay(10, 20)
     result_sub = sub.delay(20,5)
     print("result of add , result of sub", result_add.status, result_sub.status)
     return HttpResponse("Queue add  and queue sub")
